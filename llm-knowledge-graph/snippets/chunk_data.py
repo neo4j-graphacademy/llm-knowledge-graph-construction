82106b379059c3fafca8e80fa6db1a1f723b688b
Martin O'Hanlon <martin.ohanlon@neo4j.com>
2025-10-17 14:32:53 +0100

    remove python activities

--- llm-knowledge-graph/snippets/chunk_data.py
+++
@@ -1,9 +0,0 @@
-from langchain.text_splitter import CharacterTextSplitter
-
-text_splitter = CharacterTextSplitter(
-    separator="\n\n",
-    chunk_size=1500,
-    chunk_overlap=200,
-)
-
-chunks = text_splitter.split_documents(docs)
--- llm-knowledge-graph/snippets/load_data.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
-
-DOCS_PATH = "llm-knowledge-graph/data/course/pdfs"
-
-loader = DirectoryLoader(DOCS_PATH, glob="**/*.pdf", loader_cls=PyPDFLoader)
-
-docs = loader.load()
